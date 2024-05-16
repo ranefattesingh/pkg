f32718888e7fb752cd4c888cbbd48078d092ce1d
Fattesingh Rane <ranefattesingh@hotmail.com>
2024-05-16 21:51:58 +0530

    fix: worker pool

--- workerpool/worker_pool.go
+++ workerpool/worker_pool.go
@@ -2,81 +2,72 @@ package workerpool
 
 import (
 	"context"
+	"sync"
 )
 
 type workerPool struct {
 	workerCount int
 	taskFnChan  chan func(ctx context.Context) error
 	errChan     chan error
+	wg          sync.WaitGroup
 }
 
-func NewWorkerPool(wc int) *workerPool {
+func NewWorkerPool(wc int, taskBufferSize int) *workerPool {
 	return &workerPool{
 		workerCount: wc,
-		taskFnChan:  make(chan func(ctx context.Context) error, 1),
-		errChan:     make(chan error, 1),
+		taskFnChan:  make(chan func(ctx context.Context) error, taskBufferSize),
+		errChan:     make(chan error, wc), // Buffer size set to the number of workers
 	}
 }
 
-// Allows submition of new task to the pool.
+// Allows submission of a new task to the pool.
 func (wp *workerPool) AddTask(taskFn func(ctx context.Context) error) {
 	wp.taskFnChan <- taskFn
 }
 
 func (wp *workerPool) StartAndExitOnErr(ctx context.Context) error {
-	defer func() {
-		close(wp.errChan)
-		close(wp.taskFnChan)
-	}()
+	defer close(wp.errChan)
 
-	wrappedCtx, cancelFn := context.WithCancel(ctx)
-	defer cancelFn()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	wp.start(wrappedCtx)
+	wp.start(ctx)
 
-	if err := <-wp.errChan; err != nil {
-		return err
-	}
+	// Wait for all tasks to be completed
+	wp.wg.Wait()
 
-	return nil
+	return <-wp.errChan
 }
 
-func (wp *workerPool) StartAndLogOnErr(ctx context.Context) {
-	go func(taskFnChan chan func(ctx context.Context) error, errChan chan error) {
-		defer func() {
-			close(wp.errChan)
-			close(wp.taskFnChan)
-		}()
+func (wp *workerPool) StartAndIgnoreErr(ctx context.Context) {
+	defer close(wp.errChan)
 
-		wp.start(ctx)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-		for {
-			select {
-			case <-ctx.Done():
-				return
+	wp.start(ctx)
 
-			case taskFn, ok := <-wp.taskFnChan:
-				if ok {
-					if err := taskFn(ctx); err != nil {
-						errChan <- err
-					}
-				}
-			}
-
-		}
-	}(wp.taskFnChan, wp.errChan)
+	// Wait for all tasks to be completed
+	wp.wg.Wait()
 }
 
 func (wp *workerPool) start(ctx context.Context) {
+	wp.wg.Add(wp.workerCount)
 	for i := 0; i < wp.workerCount; i++ {
-		go worker(ctx, wp.taskFnChan, wp.errChan)
+		go func() {
+			defer wp.wg.Done()
+			worker(ctx, wp.taskFnChan, wp.errChan)
+		}()
 	}
 }
 
 func worker(ctx context.Context, taskFnChan <-chan func(ctx context.Context) error, errChan chan<- error) {
 	for {
 		select {
-		case taskFn := <-taskFnChan:
+		case taskFn, ok := <-taskFnChan:
+			if !ok {
+				return
+			}
 			if err := taskFn(ctx); err != nil {
 				errChan <- err
 			}
@@ -85,4 +76,8 @@ func worker(ctx context.Context, taskFnChan <-chan func(ctx context.Context) err
 			return
 		}
 	}
+}
+
+func (wp *workerPool) RecieveErr() <-chan error {
+	return wp.errChan
 }
