41e5dcc2577014cbb5cd1d83ff0883dc2f44eb80
Fattesingh Rane <ranefattesingh@hotmail.com>
2024-05-26 21:12:07 +0530

    feat: postgres migrator

---
+++ postgresql/migrate/migrate.go
@@ -0,0 +1,35 @@
+package migrate
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/ranefattesingh/pkg/postgresql"
+)
+
+type PostgresMigrator struct {
+	migrate *migrate.Migrate
+}
+
+func NewDatabaseMigrator(migrationDir string, connectionString string) (*PostgresMigrator, error) {
+	migrate, err := migrate.New("file://"+migrationDir, postgresql.EncodeConnectionString(connectionString))
+	if err != nil {
+		return nil, fmt.Errorf("migrator initialization error: %w", err)
+	}
+
+	return &PostgresMigrator{migrate: migrate}, nil
+}
+
+func (p *PostgresMigrator) Migrate() *migrate.Migrate {
+	return p.migrate
+}
+
+func (p *PostgresMigrator) CloseDatabaseMigrator() (source error, database error) {
+	return p.migrate.Close()
+}
+
+func (p *PostgresMigrator) GracefulStop() {
+	p.migrate.GracefulStop <- true
+}
