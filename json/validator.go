96bc07e7f9739dafbe7bf966b9cbf521f6f9c3e9
Fattesingh Rane <ranefattesingh@hotmail.com>
2024-05-26 22:41:52 +0530

    fix: decoder

--- json/validator.go
+++ json/validator.go
@@ -3,7 +3,6 @@ package json
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -14,9 +13,9 @@ type Validator interface {
 func DecodeAndValidateJSON[T Validator](r *http.Request) (T, error) {
 	var v T
 
-	err := json.NewDecoder(r.Body).Decode(v)
+	err := json.NewDecoder(r.Body).Decode(&v)
 	if err != nil {
-		return v, fmt.Errorf("decode json: %w", err)
+		return v, err
 	}
 
 	return v, v.Valid(r.Context())
