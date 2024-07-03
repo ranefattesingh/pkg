447188ac6ee058194cecfa07db37ad6fc7f09f5f
Fattesingh Rane <ranefattesingh@gmail.com>
2024-07-03 21:20:13 +0530

    feat: jwt token parsing

---
+++ auth/jwt/jwt.go
@@ -0,0 +1,42 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWT struct {
+	signingMethod jwt.SigningMethod
+	secretKey     string
+}
+
+func NewJWT(sm jwt.SigningMethod) *JWT {
+	return &JWT{
+		signingMethod: sm,
+	}
+}
+
+// Generate new JWT Token.
+func (j *JWT) GenerateToken(c jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(j.signingMethod, c)
+
+	// Register the JWT string
+	tokenString, err := token.SignedString([]byte(j.secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (j *JWT) Parse(ts string) (jwt.MapClaims, bool, error) {
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(ts, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+	if err != nil {
+		return nil, false, err
+	}
+
+	return claims, token.Valid, nil
+}
